df4af6d17ed2d4bdb3647dc83962d50eac691610
Ajay Tharendra <ajaytharendra@gmail.com>
2026-07-02 22:19:15 +0530

    Add Vercel Speed Insights

--- src/main.tsx
+++ src/main.tsx
@@ -4,5 +4,11 @@ import "@fontsource/geist-mono";
 import "@fontsource/instrument-serif";
 import App from "./App";
 import "./index.css";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <>
+    <App />
+    <SpeedInsights />
+  </>
+);
